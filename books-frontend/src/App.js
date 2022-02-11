0fb988fd36590359749caa1092b1b42e35463074
Drumshtick <ekodnar@outlook.com>
2022-02-10 19:03:49 -0600

    added MaterialUI Navbar

--- books-frontend/src/App.js
+++ books-frontend/src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import './App.scss';
 import Books from './pages/Books';
 import ManageBooks from './pages/ManageBooks';
+import Nav from './components/Nav';
 function App() {
   return (
       <BrowserRouter>
+      <Nav />
         <Routes>
           <Route exact path="/" element={<Books />} />
           <Route exact path='/manage'  element={<ManageBooks />} />
