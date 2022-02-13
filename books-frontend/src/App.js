d870d37e0539f8e78c3478639f45f8c7e1c4609b
Drumshtick <ekodnar@outlook.com>
2022-02-13 15:32:08 -0600

    removed nav

--- books-frontend/src/App.js
+++ books-frontend/src/App.js
@@ -1,13 +1,12 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.scss';
-import Books from './pages/Books';
+import Books from './pages/books';
 import ManageBooks from './pages/ManageBooks';
 import Nav from './components/Nav/Nav';
 function App() {
   return (
       <BrowserRouter>
-      <Nav />
         <Routes>
           <Route exact path="/" element={<Books />} />
           <Route exact path='/manage'  element={<ManageBooks />} />
