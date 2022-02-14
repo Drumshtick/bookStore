faa6f6132114ff677066e45bf26b5d4c5ea83eaa
Drumshtick <ekodnar@outlook.com>
2022-02-13 21:52:11 -0600

    imported MUI and implemented props

--- books-frontend/src/components/BooksListItem/BooksListItem.js
+++ books-frontend/src/components/BooksListItem/BooksListItem.js
@@ -1,6 +1,23 @@
+import './BooksListItem.scss';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 
-const BooksListItem = () => {
-  
+const BooksListItem = (props) => {
+  const { book, setActiveBook} = props;
+  const handleClick = () => {
+    setActiveBook({...book});
+  };
+
+  return (
+    <ListItemButton
+      sx={{color: 'white'}}
+    >
+      <ListItemText
+        primary={book.name}
+        onClick={handleClick}  
+      />
+    </ListItemButton>
+  );
 };
 
 export default BooksListItem;
