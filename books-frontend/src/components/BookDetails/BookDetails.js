07d9c0639263b0e88fc2e48d8c9b9381e769878b
Drumshtick <ekodnar@outlook.com>
2022-02-13 21:51:12 -0600

    added MUI replaced placeholders

--- books-frontend/src/components/BookDetails/BookDetails.js
+++ books-frontend/src/components/BookDetails/BookDetails.js
@@ -1,19 +1,100 @@
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import EditBook from '../EditBook/EditBook';
 import './BookDetails.scss';
 
 const BookDetails = (props) => {
+  const { activeBook } = props;
+  console.log(activeBook)
   return (
     <article>
-      <header className='details-header'>
-        <h1>Name</h1>
-        <h3>Author</h3>
-      </header>
-      <section>
-        <div>
-          <p>----ISBN----</p>
-          <p>----1994----</p>
+      <Card
+        sx={{
+          maxWidth: '95%',
+          maxHeight: '90%',
+          margin: '0 auto',
+          overflowY: 'auto'
+        }}
+      >
+        <div className='edit'>
+          <EditBook />
         </div>
-          <p>"A farm is taken over by its overworked, mistreated animals. With flaming idealism and stirring slogans, they set out to create a paradise of progress, justice, and equality. Thus the stage is set for one of the most telling satiric fables ever penned –a razor-edged fairy tale for grown-ups that records the evolution from revolution against tyranny to a totalitarianism just as terrible."</p>
-      </section>
+        <CardContent>
+        <Box
+        sx={{
+          marginTop: '20px',
+          height: 'auto',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          }}
+      >
+        <Typography
+          variant="h3"
+          color="black"
+          sx={{
+            marginBottom: '15px',
+            textAlign: 'center'
+          }}
+        >
+          {activeBook.name}
+        </Typography>
+        <Typography
+          variant="h5"
+          color="black"
+        >
+          Author: {activeBook.author}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          textAlign: 'center'
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+          }}
+        >
+          <h5 className="book-publishing-data">IBSN: {activeBook.isbn}</h5>
+          <h5 className="book-publishing-data">Year published: {activeBook.year}</h5>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            alignItems: 'start'
+          }}
+        >
+          <Typography
+                variant="p"
+                sx={{
+                  paddingLeft: '10px'
+                }}
+              >
+              Description:
+            </Typography>
+            <Typography
+              variant="p"
+              sx={{
+                padding: '20px'
+              }}
+            >
+              {activeBook.description}
+            </Typography>
+        </Box>
+      </Box>
+        </CardContent>
+      </Card>
     </article>
   );
 };
