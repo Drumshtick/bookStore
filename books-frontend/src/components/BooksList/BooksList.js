9eb345a1c90d109e7a5579c5b1cf2ddff463d2d9
Drumshtick <ekodnar@outlook.com>
2022-02-13 21:51:35 -0600

    added MUI implemented state

--- books-frontend/src/components/BooksList/BooksList.js
+++ books-frontend/src/components/BooksList/BooksList.js
@@ -1,18 +1,51 @@
-import './BooksList.scss';
-
+import './styles.scss';
+import BooksListItem from '../BooksListItem/BooksListItem';
+import List from '@mui/material/List';
+import ListSubheader from '@mui/material/ListSubheader';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 const BooksList = (props) => {
-  const { books } = props;
-  const bookTitles = books.map(book => {
-    
+  const { books, setActiveBook } = props;
+  const bookTitles = books.map((book, i) => {
+    return (
+      <BooksListItem
+        key={i}
+        book={book}
+        setActiveBook={setActiveBook}
+      />
+    );
   });
   return (
     <aside>
-      <header>
-        <h3>Books List</h3>
-      </header>
-      <section>
-
-      </section>
+      <Box
+        sx={{
+          height: '30%',
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderBottom: '1px solid rgb(45, 55, 72)'
+          }}
+      >
+        <Typography
+          variant="h3"
+          color="white"
+        >
+          Book List
+        </Typography>
+      </Box>
+      <List
+        sx={{
+          height: '100%',
+          width: '100%',
+          maxHeight: '100%',
+          overflow: 'auto'
+        }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+      >
+        {bookTitles}
+      </List>
     </aside>
   );
 };
