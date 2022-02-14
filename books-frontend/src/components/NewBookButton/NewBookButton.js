eb7f94f8686ed8e8986db343936fa90ae29d046a
Drumshtick <ekodnar@outlook.com>
2022-02-13 22:10:27 -0600

    changed name for clarity

---
+++ books-frontend/src/components/NewBookButton/NewBookButton.js
@@ -0,0 +1,38 @@
+import Button from '@mui/material/Button';
+import AddIcon from '@material-ui/icons/Add';
+import Tooltip from '@mui/material/Tooltip';
+import { useNavigate  } from "react-router-dom";
+
+const NewBookButton = () => {
+
+  const navigate  = useNavigate ();
+
+  const handleClick = () => {
+    navigate('/manage')
+  };
+
+  return (
+    <Tooltip
+      title="New Book"
+      placement="left"
+    >
+      <Button
+        sx={{
+          color: '#fff',
+          borderRadius: '100%',
+          height: '64px',
+          backgroundColor: '#333'
+        }}
+        variant="contained"
+        onClick={handleClick}
+      >
+        <AddIcon
+          fontSize='large'      
+        />
+      </Button>
+    </Tooltip>
+
+  );
+};
+
+export default NewBookButton;
