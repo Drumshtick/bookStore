fb38811b4af10718406ed41bc900071849441554
Drumshtick <ekodnar@outlook.com>
2022-02-14 19:12:54 -0600

    implemented book id as URL param

--- books-frontend/src/App.js
+++ books-frontend/src/App.js
@@ -5,11 +5,13 @@ import Books from './pages/books';
 import ManageBooks from './pages/ManageBooks';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+
   return (
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Books />} />
-          <Route exact path='/manage'  element={<ManageBooks />} />
+          <Route exact path='/edit/:id'  element={<ManageBooks />} />
+          <Route exact path='/new'  element={<ManageBooks />} />
         </Routes>
       </BrowserRouter>
   );
