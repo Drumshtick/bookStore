c05f063cb3652739c1acf3623eaec2b5f5a8ab73
Drumshtick <ekodnar@outlook.com>
2022-02-14 19:15:40 -0600

    added id's to hardcoded data

--- books-frontend/src/pages/books.js
+++ books-frontend/src/pages/books.js
@@ -7,6 +7,7 @@ import Box from '@mui/material/Box'
 
 const books = [
   {
+    id: 1,
     name: 'The Hunger Games',
     author: 'Suzanne Collins',
     isbn: '0439023483',
@@ -14,6 +15,7 @@ const books = [
     description: "In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line by forcing them all to send one boy and one girl between the ages of twelve and eighteen to participate in the annual Hunger Games, a fight to the death on live TV. Sixteen-year-old Katniss Everdeen, who lives alone with her mother and younger sister, regards it as a death sentence when she steps forward to take her sister's place in the Games. But Katniss has been close to dead before—and survival, for her, is second nature. Without really meaning to, she becomes a contender. But if she is to win, she will have to start making choices that weight survival against humanity and life against love"
   },
   {
+    id: 2,
     name: 'To Kill a Mockingbird',
     author: 'Harper Lee',
     isbn: '0446310492',
@@ -21,6 +23,7 @@ const books = [
     description: "The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it. 'To Kill A Mockingbird' became both an instant bestseller and a critical success when it was first published in 1960. It went on to win the Pulitzer Prize in 1961 and was later made into an Academy Award-winning film, also a classic."
   },
   {
+    id: 3,
     name: 'Animal Farm',
     author: 'George Orwell',
     isbn: '0451526341',
@@ -28,6 +31,7 @@ const books = [
     description: "A farm is taken over by its overworked, mistreated animals. With flaming idealism and stirring slogans, they set out to create a paradise of progress, justice, and equality. Thus the stage is set for one of the most telling satiric fables ever penned –a razor-edged fairy tale for grown-ups that records the evolution from revolution against tyranny to a totalitarianism just as terrible."
   },
   {
+    id: 4,
     name: 'The Hunger Games',
     author: 'Suzanne Collins',
     isbn: '0439023483',
@@ -35,6 +39,7 @@ const books = [
     description: "In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line by forcing them all to send one boy and one girl between the ages of twelve and eighteen to participate in the annual Hunger Games, a fight to the death on live TV. Sixteen-year-old Katniss Everdeen, who lives alone with her mother and younger sister, regards it as a death sentence when she steps forward to take her sister's place in the Games. But Katniss has been close to dead before—and survival, for her, is second nature. Without really meaning to, she becomes a contender. But if she is to win, she will have to start making choices that weight survival against humanity and life against love, In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line by forcing them all to send one boy and one girl between the ages of twelve and eighteen to participate in the annual Hunger Games, a fight to the death on live TV. Sixteen-year-old Katniss Everdeen, who lives alone with her mother and younger sister, regards it as a death sentence when she steps forward to take her sister's place in the Games. But Katniss has been close to dead before—and survival, for her, is second nature. Without really meaning to, she becomes a contender. But if she is to win, she will have to start making choices that weight survival against humanity and life against love"
   },
   {
+    id: 5,
     name: 'To Kill a Mockingbird',
     author: 'Harper Lee',
     isbn: '0446310492',
@@ -42,6 +47,7 @@ const books = [
     description: "The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it. 'To Kill A Mockingbird' became both an instant bestseller and a critical success when it was first published in 1960. It went on to win the Pulitzer Prize in 1961 and was later made into an Academy Award-winning film, also a classic."
   },
   {
+    id: 6,
     name: 'Animal Farm',
     author: 'George Orwell',
     isbn: '0451526341',
@@ -49,6 +55,7 @@ const books = [
     description: "A farm is taken over by its overworked, mistreated animals. With flaming idealism and stirring slogans, they set out to create a paradise of progress, justice, and equality. Thus the stage is set for one of the most telling satiric fables ever penned –a razor-edged fairy tale for grown-ups that records the evolution from revolution against tyranny to a totalitarianism just as terrible."
   },
   {
+    id: 7,
     name: 'The Hunger Games',
     author: 'Suzanne Collins',
     isbn: '0439023483',
@@ -56,6 +63,7 @@ const books = [
     description: "In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line by forcing them all to send one boy and one girl between the ages of twelve and eighteen to participate in the annual Hunger Games, a fight to the death on live TV. Sixteen-year-old Katniss Everdeen, who lives alone with her mother and younger sister, regards it as a death sentence when she steps forward to take her sister's place in the Games. But Katniss has been close to dead before—and survival, for her, is second nature. Without really meaning to, she becomes a contender. But if she is to win, she will have to start making choices that weight survival against humanity and life against love"
   },
   {
+    id: 8,
     name: 'To Kill a Mockingbird',
     author: 'Harper Lee',
     isbn: '0446310492',
@@ -63,6 +71,7 @@ const books = [
     description: "The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it. 'To Kill A Mockingbird' became both an instant bestseller and a critical success when it was first published in 1960. It went on to win the Pulitzer Prize in 1961 and was later made into an Academy Award-winning film, also a classic."
   },
   {
+    id: 9,
     name: 'Animal Farm',
     author: 'George Orwell',
     isbn: '0451526341',
@@ -70,6 +79,7 @@ const books = [
     description: "A farm is taken over by its overworked, mistreated animals. With flaming idealism and stirring slogans, they set out to create a paradise of progress, justice, and equality. Thus the stage is set for one of the most telling satiric fables ever penned –a razor-edged fairy tale for grown-ups that records the evolution from revolution against tyranny to a totalitarianism just as terrible."
   },
   {
+    id: 10,
     name: 'The Hunger Games',
     author: 'Suzanne Collins',
     isbn: '0439023483',
@@ -77,6 +87,7 @@ const books = [
     description: "In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line by forcing them all to send one boy and one girl between the ages of twelve and eighteen to participate in the annual Hunger Games, a fight to the death on live TV. Sixteen-year-old Katniss Everdeen, who lives alone with her mother and younger sister, regards it as a death sentence when she steps forward to take her sister's place in the Games. But Katniss has been close to dead before—and survival, for her, is second nature. Without really meaning to, she becomes a contender. But if she is to win, she will have to start making choices that weight survival against humanity and life against love"
   },
   {
+    id: 11,
     name: 'To Kill a Mockingbird',
     author: 'Harper Lee',
     isbn: '0446310492',
@@ -84,6 +95,7 @@ const books = [
     description: "The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it. 'To Kill A Mockingbird' became both an instant bestseller and a critical success when it was first published in 1960. It went on to win the Pulitzer Prize in 1961 and was later made into an Academy Award-winning film, also a classic."
   },
   {
+    id: 12,
     name: 'Animal Farm',
     author: 'George Orwell',
     isbn: '0451526341',
@@ -91,6 +103,7 @@ const books = [
     description: "A farm is taken over by its overworked, mistreated animals. With flaming idealism and stirring slogans, they set out to create a paradise of progress, justice, and equality. Thus the stage is set for one of the most telling satiric fables ever penned –a razor-edged fairy tale for grown-ups that records the evolution from revolution against tyranny to a totalitarianism just as terrible."
   },
   {
+    id: 13,
     name: 'The Hunger Games',
     author: 'Suzanne Collins',
     isbn: '0439023483',
@@ -98,6 +111,7 @@ const books = [
     description: "In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line by forcing them all to send one boy and one girl between the ages of twelve and eighteen to participate in the annual Hunger Games, a fight to the death on live TV. Sixteen-year-old Katniss Everdeen, who lives alone with her mother and younger sister, regards it as a death sentence when she steps forward to take her sister's place in the Games. But Katniss has been close to dead before—and survival, for her, is second nature. Without really meaning to, she becomes a contender. But if she is to win, she will have to start making choices that weight survival against humanity and life against love"
   },
   {
+    id: 14,
     name: 'To Kill a Mockingbird',
     author: 'Harper Lee',
     isbn: '0446310492',
@@ -105,6 +119,7 @@ const books = [
     description: "The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it. 'To Kill A Mockingbird' became both an instant bestseller and a critical success when it was first published in 1960. It went on to win the Pulitzer Prize in 1961 and was later made into an Academy Award-winning film, also a classic."
   },
   {
+    id: 15,
     name: 'Animal Farm',
     author: 'George Orwell',
     isbn: '0451526341',
@@ -116,7 +131,6 @@ const books = [
 const Books = (props) => {
   const [ activeBook, setActiveBook ] = useState({...books[0]})
 
-  console.log("active book: ", activeBook)
   return (
     <div className="books-container">
       <BooksList
