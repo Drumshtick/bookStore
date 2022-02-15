e768e6b86fcffc8146f711d4be1262468e00690c
Drumshtick <ekodnar@outlook.com>
2022-02-14 19:15:07 -0600

    implemented state for form

--- books-frontend/src/pages/ManageBooks.js
+++ books-frontend/src/pages/ManageBooks.js
@@ -1,17 +1,38 @@
+import { useState } from 'react';
+import { useNavigate, useParams } from "react-router-dom";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import './styles/manageBooks.scss';
+import Typography from '@mui/material/Typography';
+import MUIButton from '@mui/material/Button';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Typography from '@mui/material/Typography';
 import DatePicker from "react-datepicker";
+import './styles/manageBooks.scss';
 import "react-datepicker/dist/react-datepicker.css";
 const ManageBooks = () => {
+  const [ state, setState ] = useState({
+    name: '',
+    author: '',
+    isbn: '',
+    year: '',
+    description: '',
+    date: 2022
+  });
+  const { id } = useParams();
+  const navigate  = useNavigate ();
+  const handleSubmit = (e) => {
+    console.log(e)
+  };
+
+  const handleClick = (props) => {
+    navigate('/')
+  };
   return (
     <main>
       <Card
         sx={{
-          height: '90vh',
+          height: 'auto',
           width: 'auto',
           minWidth: '400px',
         }}
@@ -24,29 +45,46 @@ const ManageBooks = () => {
         >
           Create new book
         </Typography>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="bookName">
             <Form.Label>Book name</Form.Label>
-            <Form.Control type="text" />
+            <Form.Control
+              required
+              type="text"
+              value={state.name}
+              onChange={(e) => setState({...state, name: e.target.value})}
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="author">
             <Form.Label>Author</Form.Label>
-            <Form.Control type="text" />
+            <Form.Control
+              required
+              type="text"
+              value={state.author}
+              onChange={(e) => setState({...state, author: e.target.value})}
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="isbn">
-            <Form.Label>ISBN</Form.Label>
-            <Form.Control type="text" />
+            <Form.Label>ISBN&nbsp;&nbsp;(i.e 0439023483)</Form.Label>
+            <Form.Control required type="text" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="published">
-            <Form.Label>Year published</Form.Label>
-            <DatePicker className="datepicker form-control"
-              // selected={startDate}
-              // onChange={date => setStartDate(date)}
+            <Form.Label>Year published&nbsp;&nbsp;(i.e 2022)</Form.Label>
+            <DatePicker
+              className="datepicker form-control"
+              showYearPicker
+              dateFormat="yyyy"
+              selected={state.date}
+              onChange={(date) => setState({...state, date})}
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="description">
             <Form.Label>Description</Form.Label>
-            <Form.Control as="textarea" />
+            <Form.Control
+              value={state.description}
+              onChange={(e) => setState({...state, description: e.target.value})}
+              as="textarea"
+            />
           </Form.Group>
           <div className='text-center'>
             <Button variant="primary" type="submit">
@@ -56,6 +94,15 @@ const ManageBooks = () => {
           </Form>
         </CardContent>
       </Card>
+      <div className="back-btn">
+        <MUIButton
+          onClick={handleClick}
+        >
+          <ArrowBackIcon
+            fontSize="large"
+          />
+        </MUIButton>
+      </div>
     </main>
   )
 };
