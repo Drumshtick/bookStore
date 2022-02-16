ffe8c7d65bfd99171efd256fb8205165fed8ad95
Drumshtick <ekodnar@outlook.com>
2022-02-15 19:46:34 -0600

    implemented post route

--- books-backend/routes/books.js
+++ books-backend/routes/books.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { readFile, writeFile } = require('fs');
+const { readFile, writeFile, write } = require('fs');
 
 router.get('/', function(req, res) {
-  readFile('./util/data/bookData.json', 'utf-8', (err, data) => {
-    if (err) {
-      console.log("Error in ReadFile /books: ", err)
-      return res.status(400).json({ error: err })
-    }
-    return res.status(200).json(JSON.parse(data))
-  });
+  getAllBooks()
+  .then(result => {
+    return res.status(200).json(result);
+  })
+  .catch(err => {
+    console.log("Error in ReadFile /books: ", err)
+    return res.status(400).json({ error: "Error in reading data" });
+  })
 });
 
 router.get('/:id', function(req, res) {
@@ -46,28 +47,70 @@ router.get('/:id', function(req, res) {
   });
 });
 
-router.post('/', function(req, res) {
-  if (
-    !name ||
-    !author ||
-    !isbn ||
-    !year
-    ) {
-      return res.status(400).json({ error: `Missing required parameters` })
-  }
-
+router.post('/', verifyNewBookParams, function(req, res) {
   const {
     name,
     author,
     isbn,
     year
   } = req.body;
-
-  console.log(    name,
-    author,
-    isbn,
-    year)
-    return res.status(200).json({ok: 'yes'})
+  getAllBooks()
+  .then(result => {
+    const count = Object.keys(result);
+    const id = Number(count[count.length - 1]);
+    result[id + 1] = {
+      id: id + 1,
+      name,
+      author,
+      isbn,
+      year
+    }
+    writeBook(result)
+    .then(() => {
+      return res.status(200).json({ message: "success" });
+    })
+    .catch(err => {
+      console.log("Error writing book: ", err)
+      return res.status(400).json({ error: 'Failed to write book' });
+    });
+  })
 
 })
+
+function verifyNewBookParams(req, res, next) {
+  if (
+    !req.body.name ||
+    !req.body.author ||
+    !req.body.isbn ||
+    !req.body.year
+    ) {
+      console.log("Post to /books missing params")
+      return res.status(400).json({ error: `Missing required parameters` })
+  }
+
+  next();
+}
+
+function getAllBooks() {
+  return new Promise((res, rej) => {
+    readFile('./util/data/bookData.json', 'utf-8', (err, data) => {
+      if (err) {
+        rej("Error reading file: ", err);
+      }
+      res(JSON.parse(data))
+    });
+  });
+};
+
+function writeBook(book) {
+  return new Promise((res, rej) => {
+    writeFile('./util/data/bookData.json', JSON.stringify(book), err => {
+      if (err) {
+        rej("Error writing file: ", err);
+      }
+      res();
+    });
+  });
+};
+
 module.exports = router;
