1214981694517cccfc01519d3c743e77fd25f592
Drumshtick <ekodnar@outlook.com>
2022-02-16 15:36:05 -0600

    started bug fix for changing activeBook after delete

--- books-frontend/src/hooks/useBookData.js
+++ books-frontend/src/hooks/useBookData.js
@@ -6,14 +6,23 @@ export default function useBookData() {
   const [ books, setBooks ] = useState({});
   const [ activeBook, setActiveBook ] = useState({});
   
+  
   useEffect(() => {
     jsonAPI.get('/books')
     .then(result => {
-      setActiveBook(result.data[1])
+      const bookKeys = Object.keys(result.data)
+      setActiveBook(result.data[bookKeys[0]])
       setBooks(result.data)
     })
     .catch(err => console.log('Error in getting book collection'))
   }, [])
 
-  return { books, activeBook, setActiveBook };
+  useEffect(() => {
+    const bookKeys = Object.keys(books)
+    if (bookKeys.length === 0) {
+      setActiveBook({});
+    }
+  }, [books]);
+
+  return { books, activeBook, setActiveBook, setBooks };
 };
